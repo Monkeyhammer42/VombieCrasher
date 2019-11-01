c22409b7ad950922b7a533ee8da70d5cc8476811
monkeyhammer42 <michael-whiddett@hotmail.com>
2019-11-01 10:24:56 +0000

    First commit Added Level and level move/infinate level script Added player prefab, move script and shoot script/shoot.fx Added bullet script Added zombie Prefabs and Obstacle prefabs Added camera smooth follow script

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseScript
+{
+
+    private Rigidbody myBody;
+    public Transform bullet_StartPoint;
+    public GameObject bullet_Prefab;
+    public ParticleSystem shootFX;
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        ControlMovementWithKeyboard();
+        ChangeRotation();
+        ShootingControl();
+    }
+
+    void FixedUpdate()
+    {
+        
+        MoveTank();
+    }
+
+    void MoveTank()
+    {
+        myBody.MovePosition(myBody.position + speed * Time.deltaTime);
+    }
+    void ControlMovementWithKeyboard()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow)|| Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            MoveFast();
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            MoveSlow();
+        }
+        if (Input.GetKeyUp (KeyCode.LeftArrow)|| Input.GetKeyUp(KeyCode.A))
+        {
+            MoveStraight();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            MoveStraight();
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            MoveNormal();
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            MoveNormal();
+        }
+    }
+    void ChangeRotation()
+    {
+        if (speed.x > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        }else if (speed.x < 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, -maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        }else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * rotationSpeed);
+        }
+
+
+    }
+    public void ShootingControl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bullet_Prefab, bullet_StartPoint.position, Quaternion.identity);
+            bullet.GetComponent<BulletScript>().Move(2000f);
+            shootFX.Play();
+        }
+    }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
